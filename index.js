9b78e1216017aa5c94602d24282c284183b3927a
OhKh09373 <202209373@stu.uob.edu.bh>
2025-06-13 18:26:51 +0300

    styling

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ let darkmode = document.getElementById("darkmode");
 
 darkmode.addEventListener("click", changedark);
 
+let LightMode = true
+
 function changedark(){
     let body=document.querySelector("body")
     let content=document.querySelector(".content")
     let icon = document.querySelector(".mode");
 
-    if (getComputedStyle(content).backgroundColor == "rgb(255, 255, 255)"){
-        contentBackground("Black")
-        body.style.color = "white";
+    if (LightMode){
+        contentBackground("#0c111c")
+        body.style.color = "#f2f2f2";
         body.style.backgroundImage = "url('src/night brick.jpg')";
         icon.src = "src/sun.png";
+        LightMode= false
     }
     else {
-        contentBackground("White")
-        body.style.color = "black";
+        contentBackground("#f2f2f2")
+        body.style.color = "#0c111c";
         body.style.backgroundImage = "url('src/brick wall.jpg')";
         icon.src = "src/moon.png";
+        LightMode= true
     }
     
 }
@@ -25,10 +29,11 @@ function changedark(){
 window.addEventListener("DOMContentLoaded", () => {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         let body = document.querySelector("body");
-        contentBackground("Black")
-        body.style.color = "white";
+        contentBackground("#0c111c")
+        body.style.color = "#f2f2f2";
         body.style.backgroundImage = "url('src/night brick.jpg')";
         darkmode.src = "src/sun.png";
+        LightMode= false
     }
 });
 
